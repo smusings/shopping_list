497d7b747089cd6f2d24afa46974da282b1e9180
smusings <etyulmenkov@yahoo.com>
2015-11-08 15:32:15 -0500

    working item db model

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.dirname(os.path.realpath(__file__))+'/db/eugenet.db'
 
 db = SQLAlchemy(app)
@@ -9,7 +11,7 @@ db = SQLAlchemy(app)
 class Item(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(120))
-	quantity = db.Columb(db.String(120))
+	quantity = db.Column(db.String(120))
 
 	def __init__(self, name, quantity=None):
 		self.name = name
@@ -17,10 +19,13 @@ class Item(db.Model):
 			quantity = 1
 		self.quantity = quantity
 
+	def __repr__(self):
+		return '<Item %r>' %self.name
 
 @app.route('/')
 def home():
-	return render_template('Home.html')
+	items = Item.query.order_by(Item.id).all()
+	return render_template('Home.html', items = items)
 
 if __name__ == '__main__':
 	app.run(debug=True)
