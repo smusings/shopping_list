5ea5a11af7dc89ad39a92795d6a27698f65533e1
smusings <etyulmenkov@yahoo.com>
2016-02-10 20:33:22 -0500

    Added session timeout

--- shoppingList/config.py
+++ shoppingList/config.py
@@ -1,5 +1,7 @@
 import os
+from datetime import timedelta
 
 DEBUG = True
 SECRET_KEY = 'github'
+PERMANENT_SESSION_LIFETIME = timedelta(minutes=5)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.dirname(os.path.realpath(__file__))+'/db/eugenet.db'
