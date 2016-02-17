62b825999bdbef35d3686fff88482ee3c6daa76c
smusings <etyulmenkov@yahoo.com>
2016-02-16 19:39:24 -0500

    GUI changes, also about to toy around with migrations

--- shoppingList/__init__.py
+++ shoppingList/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from flask.ext.mail import Mail
 
 app = Flask(__name__)
@@ -9,5 +11,6 @@ mail = Mail(app)
 app.config.from_object('shoppingList.config')
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 import shoppingList.routes
