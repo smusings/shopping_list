09eb4f2d0ce948223b0f7232a08bfc4ba8b9b3eb
smusings <etyulmenkov@yahoo.com>
2016-02-17 19:46:01 -0500

    Added bootstrap

--- shoppingList/__init__.py
+++ shoppingList/__init__.py
@@ -4,9 +4,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask.ext.mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 mail = Mail(app)
+Bootstrap(app)
 
 app.config.from_object('shoppingList.config')
 
