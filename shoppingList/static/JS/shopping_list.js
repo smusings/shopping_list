eaf0426bae66d59f7d91a3cbfbbdfe8383821fdb
smusings <etyulmenkov@yahoo.com>
2016-02-27 20:21:55 -0500

    Moving some stuff around to make this more of a one page app

--- shoppingList/static/JS/shopping_list.js
+++ shoppingList/static/JS/shopping_list.js
@@ -1,6 +1,7 @@
 shoppingList.controller('listController', function($scope, $http){
 
 	$scope.shoppingList = []
+	$scope.items = []
 
 	$scope.getData = function()
 	{
@@ -50,6 +51,10 @@ shoppingList.controller('listController', function($scope, $http){
 
 	$scope.getList = function(id)
 	{
+		$http.get("/api/list/item"+id)
+		.success(function(response) {
+				$scope.items = response['data'];
+			});
 		// Pass in an id, and get the content, then display it in content.
 		// This way you can keep everything on one app and save page reloads.
 	}
